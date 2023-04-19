e74b71d2be87c7f32cd6eb04e69a482ec1252969
jiho9118 <mexocoxon@gmail.com>
2023-04-19 23:51:08 +0900

    setup

---
+++ review/pages/index.js
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>얌얌 기록장</title>
+        <meta name='description' content='냠냠쩝쩝 맛도리 여정' />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      <section></section>
+    </>
+  );
+}
