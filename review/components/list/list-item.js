eb736cea0e6530cc2cfe5b1a6de1842ecbc7ef71
jiho9118 <mexocoxon@gmail.com>
2023-04-21 17:00:11 +0900

    주소 링크 연결, 사진 연결

--- review/components/list/list-item.js
+++ review/components/list/list-item.js
@@ -1,7 +1,33 @@
-export default function ListItem({data}) {
-  
-  return(
-    <>
-    </>
-  )
+import Image from 'next/legacy/image';
+import Link from 'next/link';
+
+export default function ListItem({ data }) {
+  const cover = data.cover.file?.url || data.cover.external.url;
+  const name = data.properties.name.title[0].plain_text;
+  const addr = data.properties.addr.rich_text[0].plain_text;
+  const star = data.properties.star.multi_select[0].name;
+  const review = data.properties.review.rich_text[0].plain_text;
+  return (
+    <div className='item'>
+      <Image
+        src={cover}
+        alt='cover img'
+        width='100%'
+        height='60%'
+        layout='responsive'
+        objectFit='cover'
+        quality={100}
+      />
+      <div>
+        <h1 className='text-xl mt-2 font-bold'>
+          {name} ({star})
+        </h1>
+        <Link href={`https://www.google.com/maps/place/${addr}`}>
+
+        <h3 className='mt-2 text-slate-400 text-md'>{addr}</h3>
+        </Link>
+        <h3 className='mt-1 text-md'>{review}</h3>
+      </div>
+    </div>
+  );
 }
--- review/pages/list.js
+++ review/pages/list.js
@@ -1,4 +1,5 @@
 import Layout from '@/components/layout';
+import ListItem from '@/components/list/list-item';
 import { TOKEN, DATABASE_ID } from '../config';
 
 export default function List({ projects }) {
@@ -6,9 +7,9 @@ export default function List({ projects }) {
   return (
     <Layout>
       <div className='grid grid-cols-1 gap-8 p-12 m-4 md:grid-cols-2'>
-        {projects &&
-          projects.results?.map(
-            (proj) => proj.properties.name.title[0]?.plain_text
+        {
+          projects.results.map(
+            (proj) =>  <ListItem key={proj.id} data={proj} />
           )}
       </div>
     </Layout>
@@ -20,7 +21,7 @@ export async function getStaticProps() {
     method: 'POST',
     headers: {
       accept: 'application/json',
-      'Notion-Version': '2022-02-22',
+      'Notion-Version': '2022-06-28',
       'content-type': 'application/json',
       authorization: `Bearer ${TOKEN}`,
     },
@@ -34,6 +35,10 @@ export async function getStaticProps() {
     options
   );
   const projects = await res.json();
+
+  const projectName = projects.results.map(
+    (proj) => proj.properties.name.title[0]?.plain_text
+  );
 
   return {
     props: { projects },
